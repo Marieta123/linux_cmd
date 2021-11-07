9b2553b93c7d2dbd73170f36e69e643c7e73d0c7
Marieta123 <otaryan.marieta@student.rau.am>
2021-11-07 21:20:43 +0400

    copy

--- cp/cp.c
+++ cp/cp.c
@@ -1,3 +1,38 @@
-int main() {
-  return 0;
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+
+int main(int argc,char** argv){
+char buffer[1024];
+int sourcefile;
+int targetfile;
+size_t count;// byteri qanaky chapelu hamar
+
+if(argc<3){
+return 1;
+}
+sourcefile = open(argv[1],O_RDONLY);
+//O_RDONLY ov miayn bacum enq ev kardum
+if (sourcefile==-1){
+//-1 a chi bacvel,toxenq message
+printf("sourcefile is closed");
+return 1;
+
+}
+targetfile= open(argv[2],O_WRONLY|O_CREAT|O_TRUNC|S_IRUSR|S_IWUSR|S_IROTH);
+if(targetfile==-1){
+printf("targetfile id closed");
+close(sourcefile); //ete error a stacvel uremn petqa araji filey pakel
+
+return 1;
+}
+while((count=read(sourcefile,buferr,sizeof(buffer)))!=0){
+write (targetfile,buffer,count);
+}
+close(sourcefile);
+close(targetfile);
+return 0;
+
 }
