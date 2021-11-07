08e0e1d856b266d4c58c9663c6063646f402e873
Marieta123 <otaryan.marieta@student.rau.am>
2021-11-07 23:13:06 +0400

    rm

--- rm/rm.c
+++ rm/rm.c
@@ -1,3 +1,41 @@
-int main() {
-  return 0;
+#include <stdio.h>
+#include <dirent.h>
+#include <errno.h>
+#include <stdlib.h>
+int main(int argc, char* argv[])
+{
+        if (argc < 2)
+ {
+    printf("sxal\n");
+    return 1;
+        }
+
+        int f = strcmp(argv[1], "-f");
+     if (f == 0)
+        {
+           for (int i = 2; i < argc; ++i)
+           {
+               int rem = remove(argv[i]);
+            if ( rem != 0)
+               {
+                   printf("sxal\n");
+               }
+           }
+         return 0;
+       }
+        for (int i = 1; i < argc; ++i)
+       {
+           DIR* directory = opendir(argv[i]);
+           if (directory != NULL)
+           {
+               closedir(directory);
+               printf("sxal\n");
+           }
+            int rem = remove(argv[i]);
+           if (rem != 0)
+           {
+              printf("Sxal\n");
+           }
 }
+       return 0;
+  }
